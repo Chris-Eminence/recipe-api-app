4817b40881a4f0ae259ce77a3ae5d1e88f2856c8
Joseph Unomieta <lionelunomieta@gmail.com>
2023-01-27 16:04:09 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: ('Food Recipe App'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white)
+        ),
+      ),
+      home: Scaffold(
+      ),
+    );
+  }
+}
